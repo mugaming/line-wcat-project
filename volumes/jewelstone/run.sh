bd54cf9c47130fa4230be7870efee8542b9bb44c
mugaming <gaming.aggregation+github@gmail.com>
2017-09-26 16:13:24 +0900

    タイムゾーンを日本に設定

--- volumes/jewelstone/run.sh
+++ volumes/jewelstone/run.sh
@@ -4,6 +4,10 @@ if ! which curl >/dev/null 2>/dev/null; then
     apk --no-cache add curl
 fi
 
+apk --no-cache add tzdata
+cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+apk --no-cache del tzdata
+
 echo '0 7 * * * /opt/bin/notify.sh' >/var/spool/cron/crontabs/root
 echo '0 19 * * * /opt/bin/notify.sh' >>/var/spool/cron/crontabs/root
 
